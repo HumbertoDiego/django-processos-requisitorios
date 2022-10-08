4893d25ffc08a8e823b72174f430ea8d7f3f1854
HumbertoDiego <humberto-xingu@live.com>
2022-10-08 15:05:32 -0400

    copy some files

--- prs/settings.py
+++ prs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from appconfig import app, host
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w3xz7@!)cst4&i^h5c&e&chul@whi428*8i*u+mein1!ch*g+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = host["names"]
 
 
 # Application definition
--- requisicoes/views.py
+++ requisicoes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+def index(request):
+    context = {} 
+    return render(request, 'index.html', context)
