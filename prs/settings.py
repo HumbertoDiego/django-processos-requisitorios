18d0199ed6f01a41e1ef1b2abc2a7542fd736cff
HumbertoDiego <humberto-xingu@live.com>
2022-11-13 17:11:47 -0400

    update sttings

--- prs/settings.py
+++ prs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import json, os
-from appconfig import app, host, sped, ldap as ldapconf
+from appconfig import app, host, sped, ldap
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
